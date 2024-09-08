c5f2f25f31fb902255e74059150b3d74738d86d3
Amirul Hakimee <ksskso07@gmail.com>
2024-09-09 04:16:52 +0800

    update
    -firebase (fixed)
    -forgot password(Fixed)
    
    - splash screen (xleh add nnti crash, maybe sbb file type)
    - homepage salah (done ui, tinggal cantikan after dh siap func from kimi)

--- loginUI_prototypefirebase/app/build.gradle.kts
+++ loginUI_prototypefirebase/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -43,6 +44,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation ("com.google.android.gms:play-services-auth:20.0.0")
+    implementation ("com.google.firebase:firebase-auth:21.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
